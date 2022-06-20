ca7a64ad3f35ee14d60a642a99c921b8624e6f85
Irina <mandarinka.21.09.00@gmail.com>
2022-06-20 15:45:52 +0300

    Snake moves functions

--- src/Snake.tsx
+++ src/Snake.tsx
@@ -1,26 +1,70 @@
-import { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Pokemon } from './PokemonList';
 
 type Point = {
   x: number;
   y: number;
 };
+type Props = {
+  pokemonId: Pokemon['id'];
+};
 
-const GRID_SIZE = 21;
+const GRID_ROWS_SIZE = 21;
+const GRID_COLUMNS_SIZE = 25;
 
-export const SnakeStates = ({ pokemonId }: { pokemonId: Pokemon['id'] }): JSX.Element => {
+export const SnakeStates = ({ pokemonId }: Props): JSX.Element => {
+  const [speed, setSpeed] = useState(200);
   const [snakeBody, setSnakeBody] = useState<Point[]>([
-    { x: 11, y: 4 },
-    { x: 11, y: 3 },
+    { x: 4, y: 11 },
+    { x: 3, y: 11 },
   ]);
   const [food, setFood] = useState<Point>(generatePointOutSnakeBody(snakeBody));
-
+  const [direction, setDirection] = useState<Point>({ x: 1, y: 0 });
   const getElementPositionStyle = (element: Point) => {
     return {
-      gridRowStart: element.x,
-      gridColumnStart: element.y,
+      gridRowStart: element.y,
+      gridColumnStart: element.x,
     };
   };
+
+  useEffect(() => {
+    const keyPressHandler = (ev: KeyboardEvent) => {
+      switch (ev.code) {
+        case 'ArrowUp':
+          setDirection((prev) => {
+            return prev.y === 1 ? prev : { x: 0, y: -1 };
+          });
+          break;
+        case 'ArrowDown':
+          setDirection((prev) => {
+            return prev.y === -1 ? prev : { x: 0, y: 1 };
+          });
+          break;
+        case 'ArrowRight':
+          setDirection((prev) => {
+            return prev.x === -1 ? prev : { x: 1, y: 0 };
+          });
+          break;
+        case 'ArrowLeft':
+          setDirection((prev) => {
+            return prev.x === 1 ? prev : { x: -1, y: 0 };
+          });
+          break;
+      }
+    };
+    window.addEventListener('keydown', keyPressHandler);
+  }, []);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setSnakeBody((prev) => {
+        const newHeadPos: Point = outOfGameBoard({ x: prev[0].x + direction.x, y: prev[0].y + direction.y });
+        prev.length > 1 ? prev.pop() : null;
+        return [newHeadPos, ...prev];
+      });
+    }, speed);
+    return () => clearInterval(intervalId);
+  }, [direction, speed]);
 
   return (
     <div className={'game-board'}>
@@ -48,8 +92,8 @@ const generatePointOutSnakeBody = (snakeBody: Point[]) => {
   let randomPoint: Point;
   do {
     randomPoint = {
-      x: Math.floor(Math.random() * GRID_SIZE) + 1,
-      y: Math.floor(Math.random() * GRID_SIZE) + 1,
+      x: Math.floor(Math.random() * GRID_COLUMNS_SIZE) + 1,
+      y: Math.floor(Math.random() * GRID_ROWS_SIZE) + 1,
     };
   } while (isPointOutOfSnake(snakeBody, randomPoint));
 
@@ -62,4 +106,12 @@ const isPointOutOfSnake = (snake: Point[], point: Point) => {
 
 const isEaten = (snake: Point[], food: Point) => {
   return snake[0].x === food.x && snake[0].y === food.y;
+};
+
+const outOfGameBoard = (point: Point): Point => {
+  if (point.x > GRID_COLUMNS_SIZE) point.x %= GRID_COLUMNS_SIZE;
+  if (point.x < 1) point.x += GRID_COLUMNS_SIZE;
+  if (point.y > GRID_ROWS_SIZE) point.y %= GRID_ROWS_SIZE;
+  if (point.y < 1) point.y += GRID_ROWS_SIZE;
+  return point;
 };
